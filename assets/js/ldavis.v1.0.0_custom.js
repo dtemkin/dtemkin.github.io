c5149e1ad150a92e5c131597bc2999e6796e4f8d
dtemkin <just.dantastic@yandex.com>
2019-08-23 09:44:16 -0600

    fixed post

--- assets/js/ldavis.v1.0.0_custom.js
+++ assets/js/ldavis.v1.0.0_custom.js
@@ -4,7 +4,7 @@
 
 'use strict';
 
-var LDAvis = function(to_select, data_or_file_name) {
+var LDAvis = function(to_select, data_or_file_name, color1, color2) {
 
     // This section sets up the logic for event handling
     var current_clicked = {
@@ -35,9 +35,8 @@ var LDAvis = function(to_select, data_or_file_name) {
             old: 1,
             current: 1
         },
-        color1 = "#1f77b4", // baseline color for default topic circles and overall term frequencies
-        color2 = "#d62728"; // 'highlight' color for selected topics and term-topic frequencies
-
+        color1 = typeof color1 !=='undefined' ? color1 : "#1f77b4", // baseline color for default topic circles and overall term frequencies
+        color2 = typeof color2 !=='undefined' ? color2: "#d62728"; // 'highlight' color for selected topics and term-topic frequencies
     // Set the duration of each half of the transition:
     var duration = 750;
 
