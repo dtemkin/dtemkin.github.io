aa5bc7425555c5708ab91055d222b9be9cc55377
dtemkin <just.dantastic@yandex.com>
2019-08-25 00:14:07 -0600

    fix ldavis

--- assets/js/ldavis.js
+++ assets/js/ldavis.js
@@ -658,7 +658,7 @@ var LDAvis = function(to_select, data_or_file_name, color1, color2) {
             var lambdaDiv = document.createElement("div");
             lambdaDiv.setAttribute("id", lambdaInputID);
             //lambdaDiv.setAttribute("style", "padding: 5px; background-color: #e8e8e8; display: inline-block; height: 50px; width: " + lambdaDivWidth + "px; float: right; margin-right: 30px");
-            lambdaDiv.setAttribute("style", "position: relative; top: "+ (mdsheight + margin.bottom + margin.top + rMax) +"; left: 20; z-index: -1; padding: 5px; background-color: #e8e8e8; display: inline-block; height: 50px; width: " + lambdaDivWidth + "px; float: right; margin-right: 30px");
+            lambdaDiv.setAttribute("style", "position: relative; top: "+ (mdsheight + margin.bottom + margin.top + (2*rMax)) +"; left: -50; z-index: -1; padding: 5px; background-color: #e8e8e8; display: inline-block; height: 50px; width: " + lambdaDivWidth + "px; float: right; margin-right: 30px");
             inputDiv.appendChild(lambdaDiv);
 
             var lambdaZero = document.createElement("div");
@@ -1031,7 +1031,7 @@ var LDAvis = function(to_select, data_or_file_name, color1, color2) {
             d3.select("#" + barFreqsID)
                 .append("text")
                 .attr("x", barwidth/2)
-                .attr("y", -10)
+                .attr("y", -5)
                 .attr("class", "bubble-tool") //  set class so we can remove it when highlight_off is called
                 .style("text-anchor", "middle")
                 .style("font-size", "16px")
