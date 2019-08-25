f7e3f3ba1c2704c2ae8d89000904bd1dbfb3e942
dtemkin <just.dantastic@yandex.com>
2019-08-25 02:05:55 -0600

    fix ldavis

--- assets/js/ldavis.js
+++ assets/js/ldavis.js
@@ -7,7 +7,7 @@
 var LDAvis = function(to_select, data_or_file_name, color1, color2) {
 
     // This section sets up the logic for event handling
-    const content = $("#content");
+    const content = $("#ldavis_bezos.content");
     var content_width = content.width(),
         content_height = content.height();
 
